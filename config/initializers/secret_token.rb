3054cbf5710aed74774c4df3dea8eaace5e1d63b
mattleonard <matthew.leonard@colorado.edu>
2014-03-04 21:42:26 -0700

    Intial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BtiScraper::Application.config.secret_key_base = 'd7ad56f39f1b87a43716e0cf86ab41d40bca1db63d8fef12102ba0e18d494b81618f358834c6d153da5f15ed403df010036b63cf9aa861442cc96b2bbc3accd8'
