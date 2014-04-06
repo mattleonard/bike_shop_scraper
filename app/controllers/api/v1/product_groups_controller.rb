451f3e9c9dd9df69a02d57dd55e3f26211302950
mattleonard <matthew.leonard@colorado.edu>
2014-04-05 23:16:03 -0600

    Small naming issue

--- app/controllers/api/v1/product_groups_controller.rb
+++ app/controllers/api/v1/product_groups_controller.rb
@@ -1,6 +1,6 @@
 module Api
 	module V1
-		class ProductsController < ApplicationController
+		class ProductGroupsController < ApplicationController
 			before_filter :restrict_access
 			respond_to :json
 
